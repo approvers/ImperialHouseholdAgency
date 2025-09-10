62bd357dc475033e601a1a98e8694fc6800707a0
Colk-tech <master@colk.dev>
2025-09-11 00:43:23 +0900

    fix: remove direct inheriting abstract property with BaseModel

--- config.py
+++ config.py
@@ -1,12 +1,15 @@
 from pydantic import Field
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 from src.domain.config import DomainConfigIF
 from src.ui.discord.config import DiscordConfigIF
 
 
-class ImperialHouseholdAgencyConfig(BaseSettings, DomainConfigIF, DiscordConfigIF):
-    DISCORD_TOKEN: str = Field(
-        description="Discord bot token",
-        kw_only=True,
-    )
+class ImperialHouseholdAgencyConfig(DomainConfigIF, DiscordConfigIF, BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+    discord_token: str = Field(alias="DISCORD_TOKEN")
+
+    @property
+    def DISCORD_TOKEN(self) -> str:
+        return self.discord_token
