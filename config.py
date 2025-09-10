081d19faa0bc9d567c1f37ffaf8d01515b5ed93e
Colk-tech <master@colk.dev>
2025-09-11 00:04:18 +0900

    add: add `DISCORD_TOKEN` in `ImperialHouseholdAgencyConfig`

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
+from pydantic import Field
 from pydantic_settings import BaseSettings
 
 from src.domain.config import DomainConfigIF
+from src.ui.discord.config import DiscordConfigIF
 
 
-class ImperialHouseholdAgencyConfig(DomainConfigIF, BaseSettings):
-    pass
+class ImperialHouseholdAgencyConfig(DomainConfigIF, DiscordConfigIF, BaseSettings):
+    DISCORD_TOKEN: str = Field(
+        description="Discord bot token",
+    )
