74a9c0bb2844456a59cb32fc2bf3d24782bbb1a0
Colk-tech <master@colk.dev>
2025-09-13 19:03:51 +0900

    fix: fix of ruff format

--- src/infrastructure/repository/sqlalchemy/model/all.py
+++ src/infrastructure/repository/sqlalchemy/model/all.py
@@ -4,4 +4,4 @@ from src.infrastructure.repository.sqlalchemy.model.baes import Base
 from src.infrastructure.repository.sqlalchemy.model.messenger import Messenger
 from src.infrastructure.repository.sqlalchemy.model.user import User
 
-MODELS: Final[tuple[type[Base], ...]] = (Messenger,User)
+MODELS: Final[tuple[type[Base], ...]] = (Messenger, User)
