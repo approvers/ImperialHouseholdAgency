e4a4681a578946327ade6101316f40a82b20f144
Colk-tech <master@colk.dev>
2025-09-13 13:04:47 +0900

    add: add `ULIDMixin`, `CreatedAtMixin` and `UpdatedAtMixin` in `base.py`

--- src/infrastructure/repository/sqlalchemy/model/baes.py
+++ src/infrastructure/repository/sqlalchemy/model/baes.py
@@ -1,5 +1,27 @@
-from sqlalchemy.orm import DeclarativeBase
+from datetime import datetime
+
+from sqlalchemy import DateTime
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from ulid import ULID
+
+from src.infrastructure.repository.sqlalchemy.type.ulid import ULIDColumn
 
 
 class Base(DeclarativeBase):
     pass
+
+
+class ULIDMixin:
+    id: Mapped[ULID] = mapped_column(ULIDColumn(), primary_key=True)
+
+
+class CreatedAtMixin:
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), nullable=False
+    )
+
+
+class UpdatedAtMixin:
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), nullable=False
+    )
