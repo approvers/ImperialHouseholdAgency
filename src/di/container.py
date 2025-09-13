6acf810a163fe9359e5029dc97bf0b095d63072b
Colk-tech <master@colk.dev>
2025-09-13 22:01:23 +0900

    add: add binding `PydanticSQLAlchemyConfigModule`

--- src/di/container.py
+++ src/di/container.py
@@ -3,12 +3,16 @@ from typing import Final, Iterable
 from injector import Injector, Module
 
 from src.di.module.domain.config.pydantic import PydanticDomainConfigModule
+from src.di.module.infrastructure.sqlalchemy.config.pydantic import (
+    PydanticSQLAlchemyConfigModule,
+)
 from src.di.module.ui.discord.config.pydantic import PydanticDiscordConfigModule
 
 # NOTE:
 #   Change here to change dependencies to load!
 __MODULES: Final[Iterable[Module]] = (
     PydanticDomainConfigModule(),
+    PydanticSQLAlchemyConfigModule(),
     PydanticDiscordConfigModule(),
 )
 
