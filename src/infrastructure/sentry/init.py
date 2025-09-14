9bffce3fb4051eeca728a2056c005a543af857e3
Colk-tech <master@colk.dev>
2025-09-15 06:26:43 +0900

    fix: add `""` for `SENTRY_DSN` in `test.env`

--- src/infrastructure/sentry/init.py
+++ src/infrastructure/sentry/init.py
@@ -5,7 +5,7 @@ from src.infrastructure.sentry.config import SentryConfigIF
 
 
 def init_sentry() -> None:
-    config = DIContainer.get(SentryConfigIF)
+    config = DIContainer.get(SentryConfigIF) # type: ignore [type-abstract]
 
     sentry_sdk.init(
         dsn=str(config.SENTRY_DSN),
