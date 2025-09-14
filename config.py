086f870cb92a85d30ef77a08f62897501d714815
Colk-tech <master@colk.dev>
2025-09-15 05:47:51 +0900

    add: add `SENTRY_DSN` and `SENTRY_ENV` for `BaseConfig`

--- config.py
+++ config.py
@@ -5,10 +5,13 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 from src.domain.config import DomainConfigIF, EnvironmentEnum
 from src.infrastructure.repository.sqlalchemy.config import SQLAlchemyConfigIF
+from src.infrastructure.sentry.config import SentryConfigIF
 from src.ui.discord.config import DiscordConfigIF
 
 
-class BaseConfig(DomainConfigIF, SQLAlchemyConfigIF, DiscordConfigIF, BaseSettings):
+class BaseConfig(
+    DomainConfigIF, SQLAlchemyConfigIF, DiscordConfigIF, SentryConfigIF, BaseSettings
+):
     model_config = SettingsConfigDict(
         env_file="./env/test.env", env_file_encoding="utf-8", extra="allow"
     )
@@ -33,6 +36,18 @@ class BaseConfig(DomainConfigIF, SQLAlchemyConfigIF, DiscordConfigIF, BaseSettin
     @property
     def DISCORD_TOKEN(self) -> str:
         return self.discord_token
+
+    # Sentry
+    sentry_dsn: str = Field(alias="SENTRY_DSN")
+    sentry_env: str = Field(alias="SENTRY_ENV")
+
+    @property
+    def SENTRY_DSN(self) -> str:
+        return self.sentry_dsn
+
+    @property
+    def SENTRY_ENV(self) -> str:
+        return self.sentry_env
 
 
 class TestConfig(BaseConfig):
