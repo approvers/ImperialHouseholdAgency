b3cb004abb9361be9d626cdfe42663176fec364e
Colk-tech <master@colk.dev>
2025-09-14 16:56:43 +0900

    add: add missing `UserCreatedAt`

--- src/domain/value/user.py
+++ src/domain/value/user.py
@@ -1,10 +1,15 @@
 from pydantic import RootModel
 
 from src.domain.value.base.identifier import ULIDBase
+from src.domain.value.base.time import CreatedAtBase
 from src.domain.value.messenger import MessengerRecordID
 
 
 class UserRecordID(ULIDBase):
+    pass
+
+
+class UserCreatedAt(CreatedAtBase):
     pass
 
 
