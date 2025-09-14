a6520a0e85a789f73832c48e78a619b37cff254f
Colk-tech <master@colk.dev>
2025-09-15 00:20:06 +0900

    add: add data models for responses from repository and their test

---
+++ src/domain/interface/repository/common/response.py
@@ -0,0 +1,44 @@
+from enum import StrEnum
+from typing import TypeVar, Self, Iterable
+
+from pydantic import BaseModel, Field, model_validator
+
+from src.domain.model.base import DomainModelBase
+
+ResponseT = TypeVar("ResponseT", bound=DomainModelBase | Iterable[DomainModelBase])
+
+
+class RepositoryResultStatusEnum(StrEnum):
+    SUCCESS = "SUCCESS"
+    ERROR = "ERROR"
+
+
+class RepositoryResponseStatusEnum(StrEnum):
+    CREATED = "CREATED"
+    READ = "READ"
+    UPDATED = "UPDATED"
+    DELETED = "DELETED"
+    FAILED = "FAILED"
+
+
+class RepositoryFailedResponseEnum(StrEnum):
+    UNKNOWN = "UNKNOWN"
+
+
+class RepositoryResponse[ModelT](BaseModel):
+    response: ModelT | None
+    is_success: RepositoryResultStatusEnum
+    status: RepositoryResponseStatusEnum
+    reason: RepositoryFailedResponseEnum | None = Field(
+        default=RepositoryFailedResponseEnum.UNKNOWN
+    )
+    message: str | None = Field(default=None)
+
+    @model_validator(mode="after")
+    def must_have_message_when_failed(self) -> Self:
+        if self.is_success == RepositoryResultStatusEnum.ERROR and not self.message:
+            raise ValueError(
+                "'self.reason' is required when 'self.is_success' is set to 'ERROR'"
+            )
+
+        return self
