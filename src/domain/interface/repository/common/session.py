a1f763772262e17f20fdfc4cff80958ee904d8ec
Colk-tech <master@colk.dev>
2025-09-16 02:55:47 +0900

    add: add `AsyncSessionIF` and `AsyncSessionProvider`

--- src/domain/interface/repository/common/session.py
+++ src/domain/interface/repository/common/session.py
@@ -1,11 +1,16 @@
 from abc import abstractmethod
-from typing import Generator
+from typing import AsyncGenerator, Generator
 
 from src.common.interface import Interface
 
 
 class SessionIF(Interface):
     pass
+
+
+class AsyncSessionIF(SessionIF):
+    pass
+
 
 class SessionProvider[SessionT: SessionIF](Interface, Generator):
     @abstractmethod
@@ -14,4 +19,14 @@ class SessionProvider[SessionT: SessionIF](Interface, Generator):
 
     @abstractmethod
     def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
+
+class AsyncSessionProvider[SessionT: SessionIF](Interface, AsyncGenerator):
+    @abstractmethod
+    async def __aenter__(self) -> SessionIF:
+        pass
+
+    @abstractmethod
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
         pass
