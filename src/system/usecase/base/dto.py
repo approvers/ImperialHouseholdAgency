ca93933579fb05f342d45c3a4dae544935b34f6c
Colk-tech <master@colk.dev>
2026-01-02 19:53:06 +0900

    add: define `UsecaseRequest`, `UsecaseResponse` and `UsecaseIf`

---
+++ src/system/usecase/base/dto.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+
+
+class UsecaseBaseDTO(BaseModel):
+    pass
+
+
+class UsecaseRequest(UsecaseBaseDTO):
+    pass
+
+
+class UsecaseResponse(UsecaseBaseDTO):
+    pass
