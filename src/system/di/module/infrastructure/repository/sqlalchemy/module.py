4231501962eb2c76751421cef60df11f827bd291
Colk-tech <master@colk.dev>
2026-01-04 12:33:28 +0900

    add: add `SARepositoryModule`

---
+++ src/system/di/module/infrastructure/repository/sqlalchemy/module.py
@@ -0,0 +1,37 @@
+from injector import Module, Binder, singleton, provider
+from sqlalchemy.ext.asyncio import (
+    async_sessionmaker,
+    AsyncSession,
+    AsyncEngine,
+    create_async_engine,
+)
+
+from src.system.domain.interface.repository.messenger import MessengerRepository
+from src.system.infrastructure.repository.sqlalchemy.config import SQLAlchemyConfigIf
+from src.system.infrastructure.repository.sqlalchemy.crud.messenger import (
+    SAMessengerRepository,
+)
+
+
+class SARepositoryModule(Module):
+    _engine: AsyncEngine | None = None
+
+    @provider
+    @singleton
+    def provide_sa_async_session_factory(
+        self, config: SQLAlchemyConfigIf
+    ) -> async_sessionmaker[AsyncSession]:
+        if SARepositoryModule._engine is None:
+            SARepositoryModule._engine = create_async_engine(config.DATABASE_URL)
+
+        session_maker = async_sessionmaker(
+            SARepositoryModule._engine, expire_on_commit=False
+        )
+        return session_maker
+
+    def configure(self, binder: Binder) -> None:
+        binder.bind(
+            interface=MessengerRepository,
+            to=SAMessengerRepository,
+            scope=singleton,
+        )
