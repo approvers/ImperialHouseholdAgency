e0c35cbe8c6b61b07d7622c32660a60843a6ffb6
Colk-tech <master@colk.dev>
2026-01-04 12:31:13 +0900

    add: define `UserRepository().get_by_user_id_and_messenger()`

--- src/system/domain/interface/repository/user.py
+++ src/system/domain/interface/repository/user.py
@@ -3,7 +3,7 @@ from abc import abstractmethod
 from src.system.domain.interface.repository.common.base import RepositoryBase
 from src.system.domain.interface.repository.common.response import RepositoryResponse
 from src.system.domain.model.user import User
-from src.system.domain.value.user import UserRecordID
+from src.system.domain.value.user import UserRecordID, UserID, UserMessengerRecordID
 
 
 class UserRepository(RepositoryBase):
@@ -13,6 +13,12 @@ class UserRepository(RepositoryBase):
 
     @abstractmethod
     async def get(self, record_id: UserRecordID) -> RepositoryResponse[User | None]:
+        pass
+
+    @abstractmethod
+    async def get_by_user_id_and_messenger(
+        self, user_id: UserID, messenger_record_id: UserMessengerRecordID
+    ) -> RepositoryResponse[User | None]:
         pass
 
     async def get_or_create(self, user: User) -> RepositoryResponse[User | None]:
