b8c150e2c286e6068182cb3801d89f43c4c72cde
Colk-tech <master@colk.dev>
2026-07-28 01:30:52 +0900

    fix: evaluate ULID timestamps at call time

--- src/system/util/id.py
+++ src/system/util/id.py
@@ -5,5 +5,7 @@ from ulid import ULID
 from src.system.util.datetime import utcnow
 
 
-def generate_ulid(value: datetime.datetime = utcnow()) -> ULID:
+def generate_ulid(value: datetime.datetime | None = None) -> ULID:
+    value = value or utcnow()
+
     return ULID.from_datetime(value)
